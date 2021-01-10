0367daa9d26214143bd59c92252e80cbed991d1a
Conway <yawnoc@pm.me>
2021-01-11 02:03:49 +0800

    Tweak animate_tumbling demo parameters again

--- animate_tumbling.m
+++ animate_tumbling.m
@@ -111,5 +111,5 @@ endfunction
 
 
 %!demo
-%!  initialVertices = [0.8; 0.7i; -0.6 - 0.6i; 0.2 - 0.9i];
+%!  initialVertices = [0.8; 0.7i; -0.65 + 0.1i; 0.2 - 0.9i];
 %!  animate_tumbling ("demo.gif", initialVertices, 2 * pi);
